afd251bb165fef0771edceaaf2734719eb85b3ae
Sergey Gusev <sergus3239@gmail.com>
2024-09-08 23:00:10 +0300

    first iteration

---
+++ HW1/task2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>            
+int main(void)                      
+{
+/*
+Let’s
+    go
+	 to walk
+*/                                     
+    printf(" \n");
+    printf("Let's \n");
+    printf("    go \n");
+    printf("     to walk \n");
+     printf(" \n");
+    return 0;                      
+}                                   
