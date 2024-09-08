15d82da4b3f1e21a5cf5887c99ffba45529e6bd3
Sergey Gusev <sergus3239@gmail.com>
2024-09-08 23:02:05 +0300

    + task3

---
+++ HW1/task3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>                  
+int main(void)                      
+{                                   
+    printf("\n");                   
+    printf("    * \n"); 
+    printf("   *** \n");
+    printf("  ***** \n");
+    printf(" ******* \n");
+    printf("  HHOHH \n");
+    printf("  ZZZZZ \n");
+    printf("\n");                  
+    return 0;                       
+}                                  
